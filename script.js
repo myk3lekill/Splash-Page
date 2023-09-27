f9cc53e6c8abd18cfaec6d463f5c656b0d3092e3
me <me@gmail.com>
2023-09-27 10:00:46 +0200

    fourth commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('javascript is connected')
+// DOM
+const { body } = document;
+
+// FUNCTIONS
+function changeBackground(number) {
+    //Check if background is already showing
+    let previousBackground;
+    if (body.className) {
+        previousBackground = body.className;
+    }
+    //Select Background
+    body.className = ''; //Reset the CSS for the body when function is running
+    switch (number) {
+        case '1':
+            return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
+        case '2':
+            return (previousBackground === 'background-2' ? false : body.classList.add('background-2'));
+        case '3':
+            return (previousBackground === 'background-3' ? false : body.classList.add('background-3'));
+        default:
+            break;
+    }
+}
